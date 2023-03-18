9a27ec4867d95a8092d8e60d54b60f1e6b7ba1dc
bbmani <balajibharatwajmanikandan>
2023-03-17 21:25:15 -0400

    v1.1: Standardising Python Code

--- port-scanner.py
+++ port-scanner.py
@@ -1,7 +1,7 @@
 import sys
 import socket
 import re
-
+# -------------------------------------------------------------
 def is_IPv4(ip_address):
     ipv4 = r"(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])"
     pattern_ipv4 = re.compile(r'^(' + ipv4 + r'\.){3}' + ipv4 + r'$')
@@ -10,34 +10,43 @@ def is_IPv4(ip_address):
         return 1
     else:
         return 0
+# -------------------------------------------------------------
+def port_scanner(target):
+    try:
+        for port in range(1, 65536):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(1)
+            result = s.connect_ex((target, port))
+            if result == 0:
+                print(f"Port {port} is open")
+            s.close() 
 
-if len(sys.argv) == 2:
-    if is_IPv4(sys.argv[1]) :
-        target = sys.argv[1]
-    else:
-        print("The IP address structure is not valid")
+    except KeyboardInterrupt:
+        print("\n Exiting Program...")
         sys.exit()
-else:
-    print("Invalid Amount of Arguments. Refer the README for syntax")
-    sys.exit()
-  
-try:
-    for port in range(1, 65536):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
-        result = s.connect_ex((target, port))
-        if result == 0:
-            print(f"Port {port} is open")
-        s.close() 
 
-except KeyboardInterrupt:
-    print("\n Exiting Program...")
-    sys.exit()
+    except socket.gaierror:
+        print("Host anme cannot be resolved")
+        sys.exit()
 
-except socket.gaierror:
-    print("Host anme cannot be resolved")
-    sys.exit()
-
-except socket.error:
-    print("Couldnt connect to server")
-    sys.exit()
+    except socket.error:
+        print("Couldnt connect to server")
+        sys.exit()
+    
+# -------------------------------------------------------------
+def get_target():
+    if len(sys.argv) == 2:
+        if is_IPv4(sys.argv[1]) :
+            return sys.argv[1]
+        else:
+            print("The IP address structure is not valid")
+            sys.exit()
+    else:
+        print("Invalid Amount of Arguments. Refer the README for syntax")
+        sys.exit()
+def main():
+    
+    port_scanner(get_target())
+    
+if __name__ == "__main__" : 
+    main()
